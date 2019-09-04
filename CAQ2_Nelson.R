4660c4ecaf8311a9615ea89694a517f17f23fd55
Brian Sum <sumkh2@gmail.com>
2019-09-05 00:08:38 +0800

    EDA

--- CAQ2_Nelson.R
+++ CAQ2_Nelson.R
@@ -1,32 +1,93 @@
 pacman::p_load(dplyr, tidyverse, ggplot2, lubridate, reshape2, stringr, car, caret, ggpubr)
 
-setwd("C:/Users/nelso/Documents/Masters/EBA5002/CA Doc/data")
+#setwd("C:/Users/nelso/Documents/Masters/EBA5002/CA Doc/data")
 
 loans_all = read.csv("loans.csv", as.is = TRUE)
 summary(loans_all)
 
-#check if all lines are filled
+# check if all lines are filled
+# remove data with missing values since instances of NA is not significant
 loans = loans_all[complete.cases(loans_all),]
 glimpse(loans)
+summary(loans)
 
-#clean lines
-loans$emplength %>%
-  str_replace("< 1", "0") %>%
-  str_extract("\\d{1,2}") -> loans$emplength
+# Inspecting the Variables
+#install.packages("inspectdf")
+inspectdf::inspect_cat(loans) %>% show_plot()
+inspect_num(loans_all) %>% show_plot()
 
+# clean emplength and term lines
+table(loans$term, useNA = "always")
 loans$term %>%
   str_extract("\\d{1,2}") -> loans$term
 
-loans[,c("creditpolicy", "grade", "homeownership", "verificationstatus","targetloanstatus","purpose")] = 
-  lapply(loans[,c("creditpolicy", "grade", "homeownership", "verificationstatus","targetloanstatus","purpose")], as.factor)
+table(loans$emplength, useNA = "always")
+loans$emplength %>%
+  str_replace("< 1", "0") %>%
+  str_extract("\\d{1,2}") -> loans$emplength
 loans[,c("term","emplength")] = 
   lapply(loans[,c("term","emplength")], as.integer)
+loans = na.omit(loans)
 
+
+# Create 2 new variables for emp10years and delinq2ears
 loans = loans %>%
   mutate(emp10years = as.factor(case_when(emplength > 9 ~ "Y",
                                           emplength <= 9 ~ "N")),
          delin2years = factor(case_when(delinq2yrs > 0 ~ "Y",
-                                        delinq2yrs == 0 ~ "N")))
+                                        delinq2yrs == 0 ~ "N")),
+         homeowner = factor(ifelse(homeownership == "MORTGAGE"|homeownership == "OWN",
+                                   "Y", "N")))
+
+table(loans$emp10years, loans$emplength)
+table(loans$delin2years, loans$delinq2yrs)
+table(loans$homeowner, loans$homeownership)
+
+
+# Binning annualinc
+# install.packages("dlookr")
+library(dlookr)
+loans$annualinc_bin = dlookr::binning(loans$annualinc, nbins = 5, type = "quantile", 
+                                      ordered = T)
+table(loans$annualinc_bin, useNA = "always")
+class(loans$annualinc_bin)
+str(loans$annualinc_bin)
+
+loans %>% 
+  group_by(annualinc_bin) %>%
+  summarise(min(annualinc), max(annualinc))
+
+# Recoding verification status
+table(loans$verificationstatus, useNA = "always")
+loans$verified = factor(ifelse(loans$verificationstatus == "Not Verified", "N","Y"))
+table(loans$verified, loans$verificationstatus)
+
+# Factorise catagorical variables
+loans[,c("creditpolicy", "grade", "homeownership", "verificationstatus","targetloanstatus","purpose")] = 
+  lapply(loans[,c("creditpolicy", "grade", "homeownership", "verificationstatus","targetloanstatus","purpose")], as.factor)
+
+
+# Selecting Features for Modelling
+loans_df = select(loans, -c("id","homeownership","annualinc","verificationstatus","delinq2yrs"))
+glimpse(loans_df)
+
+# Visualisation of Correlation of Numeerical Variables
+plot_correlate(loans_df)
+
+# Generating Reports with dlookr packages
+loans_df %>%
+  diagnose_report(output_format = "html", 
+                  output_file = "Diagn.html", 
+                  output_dir = ".")
+
+loans_df %>%
+  eda_report(target = targetloanstatus, output_format = "html", 
+             output_file = "EDA.html", 
+             output_dir = ".")
+
+
+
+# Modelling ------------------------------------------------------------------#
 
 #assume that targetloanstatus defines defaults -> defaulted loans: targetloanstatus == 1
 glimpse(loans)
@@ -126,5 +187,9 @@ trainset %>%
 trainset %>%
   group_by(targetloanstatus) %>%
   count()
+
+
+
+
 
 
